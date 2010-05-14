c3024dd9ebc77437bab66fdcb9850c947713db55
Victor Costan <costan@gmail.com>
2010-05-14 18:09:56 -0400

    Sketch for hash tree.

---
+++ lib/scratchpad/models/hash_tree.rb
@@ -0,0 +1,21 @@
+# :nodoc: namespace
+module Scratchpad
+
+# :nodoc: namespace
+module Models
+
+
+# Model for an authentication tree.
+class HashTree
+  # Creates an empty hash tree.
+  #
+  # Args:
+  #   leaf_count:: the number of leaf nodes in the tree.
+  def initialize(leaf_count, default_hash)
+    
+  end
+end  # class Scratchpad::Models::HashTree
+
+end  # namespace Scratchpad::Models
+
+end  # namespace Scratchpad
