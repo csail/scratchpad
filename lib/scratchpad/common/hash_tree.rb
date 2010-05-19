98993e5ffebad1f08d5fb18717ea04bd0cbd4ce7
Victor Costan <costan@gmail.com>
2010-05-19 18:14:11 -0400

    Shell for the cache driver.

--- lib/scratchpad/common/hash_tree.rb
+++ lib/scratchpad/common/hash_tree.rb
@@ -1,5 +1,3 @@
-require 'set'
-
 # :nodoc: namespace
 module Scratchpad
 
---
+++ lib/scratchpad/common/hash_tree_cache_driver.rb
@@ -0,0 +1,13 @@
+require 'set'
+
+# :nodoc: namespace
+module Scratchpad
+
+
+# Manages a HashTreeCache.
+class HashTreeCacheDriver
+  
+  
+end  # class Scratchpad::HashTreeCacheDriver
+
+end  # namespace Scratchpad
